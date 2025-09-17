78b079a36bc197c7a96df76c5bf75a991b85911a
kenaw marie <204995551+KenawMarie@users.noreply.github.com>
2025-09-17 08:46:26 -0900

    add an event llistener for the buttons to execute the computation  and add class to the html file

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
             // DOM element selection
 const display=document.querySelector('.display')
-display.textContent=12345
-const btns=document.querySelectorAll('button')
+const btns=document.querySelectorAll('.digit,.operator')
+const equal=document.querySelector('.equal-to')
+const clear=document.querySelector('.erase')
+const bracket=document.querySelector('.bracket');
+const operators=document.querySelectorAll('.operator')
+const dot=document.querySelector('.dot')
             //functions
 function add(a,b){
   return a + b
@@ -29,11 +33,73 @@ function operate(operator,numOne,numTwo){
      return   divide(numOne,numTwo)
     }
 }
-
+let operator=['+','−','×','÷']
             // event listener
+            
+clear.addEventListener('click',()=>{
+  display.textContent=''
+})
+let count=0
+bracket.addEventListener('click',(e)=>{
+  count++;
+  if(count%2!==0){
+    display.textContent +=e.target.textContent[0]
+    console.log(count);
+}
+else{
+  display.textContent +=e.target.textContent[1]
+
+  console.log(count);
+}
+})
+
 for(const btn of btns){
     btn.addEventListener('click',(e)=>{
-       let content= display.textContent=e.target.textContent;
-       console.log(content);
+      let arr=[]
+        display.textContent += e.target.textContent;
+          arr.push(display.textContent);
+          let trimmed=arr.join().trim()
+          operator
+          if(trimmed.includes('+')){
+              let number=trimmed.split('+');
+              let number1=Number(number[0])
+              let number2=Number(number[1])
+              equal.addEventListener('click',()=>{
+                let result=  operate('+',number1,number2)
+                display.textContent=result
+                console.log(result);
+              })
+            }
+          else if(trimmed.includes('−')){
+                let number=trimmed.split('−');
+              let number1=Number(number[0])
+              let number2=Number(number[1])
+              equal.addEventListener('click',()=>{
+                let result=  operate('−',number1,number2)
+                display.textContent=result
+                console.log(result);
+              })
+            }
+          else if(trimmed.includes('×')){
+            let number=trimmed.split('×');
+              let number1=Number(number[0])
+              let number2=Number(number[1])
+              equal.addEventListener('click',()=>{
+                let result=  operate('×',number1,number2)
+                display.textContent=result
+                console.log(result);
+              })
+            }
+          else if(trimmed.includes('÷')){
+            let number=trimmed.split('÷');
+              let number1=Number(number[0])
+              let number2=Number(number[1])
+              equal.addEventListener('click',()=>{
+                let result=  operate('÷',number1,number2)
+                display.textContent=result
+                console.log(result);
+              })
+            }
+          
     })
 }
